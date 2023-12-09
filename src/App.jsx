0755ca98c7c301b9a9a1f1f8ff783c354d364f50
Sergey87 <serge.techno@gmail.com>
2023-12-09 15:58:47 +0300

    added firs component Button

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,12 @@
 import './App.css';
+import Button from './components/Button/Button';
 
 function App() {
-  return <></>;
+  return (
+    <>
+      <Button />
+    </>
+  );
 }
 
 export default App;
---
+++ src/components/Button/Button.jsx
@@ -0,0 +1,11 @@
+import './Button.css';
+
+function Button() {
+  return (
+    <>
+      <button className='button accent'>Сохранить</button>
+    </>
+  );
+}
+
+export default Button;
