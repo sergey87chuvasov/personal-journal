87ccff26e78d29b4f063bb9837cc2a810b81e7f5
Sergey87 <serge.techno@gmail.com>
2023-12-09 16:30:25 +0300

    added JournalItem component

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,13 @@
 import './App.css';
 import Button from './components/Button/Button';
+import JournalItem from './components/JournalItem/JournalItem.Jsx';
 
 function App() {
   return (
     <>
+      <h1>TITLE</h1>
       <Button />
+      <JournalItem />
     </>
   );
 }
