1be6a783d2edcb2654e641d77205f8f06253ed83
Sergey87 <serge.techno@gmail.com>
2023-12-13 21:28:29 +0300

    added elements in project

--- src/App.jsx
+++ src/App.jsx
@@ -8,25 +8,39 @@ import JournalList from './components/JournalList/JournalList';
 import JournalAddButoon from './components/JournalAddButoon/JournalAddButoon';
 import LeftPanel from './layouts/LeftPanel/LeftPanel';
 import JournalForm from './components/JournalForm/JournalForm';
+import { useState } from 'react';
+
+const INITIAL_DATA = [
+  {
+    title: 'Подготовка к обновлению курсов',
+    text: 'Горные походы открывают удивительные природные ландшафты!',
+    date: new Date(),
+  },
+  {
+    title: 'Разработка к обновлению тем',
+    text: 'Никогда не говори никогда!',
+    date: new Date(),
+  },
+  {
+    title: 'Привет, как дела?',
+    text: 'Я помню чудное мгновенье',
+    date: new Date(),
+  },
+];
 
 function App() {
-  const data = [
-    {
-      title: 'Подготовка к обновлению курсов',
-      text: 'Горные походы открывают удивительные природные ландшафты!',
-      date: new Date(),
-    },
-    {
-      title: 'Разработка к обновлению тем',
-      text: 'Никогда не говори никогда!',
-      date: new Date(),
-    },
-    {
-      title: 'Привет, как дела?',
-      text: 'Я помню чудное мгновенье',
-      date: new Date(),
-    },
-  ];
+  const [items, setItems] = useState(INITIAL_DATA);
+
+  const addItem = (item) => {
+    setItems((oldItems) => [
+      ...oldItems,
+      {
+        text: item.text,
+        title: item.title,
+        date: new Date(item.date),
+      },
+    ]);
+  };
 
   return (
     <div className='app'>
@@ -34,7 +48,7 @@ function App() {
         <Header />
         <JournalAddButoon />
         <JournalList>
-          {data.map((el) => (
+          {items.map((el) => (
             <CardButton>
               <JournalItem title={el.title} text={el.text} data={el.date} />
             </CardButton>
@@ -42,7 +56,7 @@ function App() {
         </JournalList>
       </LeftPanel>
       <Body>
-        <JournalForm />
+        <JournalForm onSubmit={addItem} />
       </Body>
     </div>
   );
--- src/components/JournalForm/JournalForm.jsx
+++ src/components/JournalForm/JournalForm.jsx
@@ -1,30 +1,22 @@
 /* eslint-disable indent */
-import { useState } from 'react';
 import Button from '../Button/Button';
 import './JournalForm.css';
 
-function JournalForm() {
-  const [inputData, setInputData] = useState('');
-
-  const inputChange = (e) => {
-    setInputData(e.target.value);
-  };
-
+function JournalForm({ onSubmit }) {
   const addJournalItem = (e) => {
     const formData = new FormData(e.target);
     const formProps = Object.fromEntries(formData);
     e.preventDefault();
-    console.log(e);
-    console.log(formProps);
+    onSubmit(formProps);
   };
 
   return (
     <form className='journal-form' onSubmit={addJournalItem}>
       <input type='text' name='title' />
       <input type='date' name='date' />
-      <input type='text' name='tag' value={inputData} onChange={inputChange} />
-      <textarea name='post' id='' cols='30' rows='10'></textarea>
-      <Button text='Сохранить' onClick={() => console.log('click')} />
+      <input type='text' name='tag' />
+      <textarea name='text' cols='30' rows='10'></textarea>
+      <Button text='Сохранить' />
     </form>
   );
 }
