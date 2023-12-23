54cc3e5ec29112e23d8e48c8287121b047dfb099
Sergey87 <serge.techno@gmail.com>
2023-12-23 23:51:59 +0300

    added the creation of my own hook to the project

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,4 @@
 /* eslint-disable indent */
-import { useState, useEffect } from 'react';
 import './App.css';
 import Header from './components/Header/Header';
 import Body from './layouts/Body/Body';
@@ -7,37 +6,29 @@ import JournalList from './components/JournalList/JournalList';
 import JournalAddButoon from './components/JournalAddButoon/JournalAddButoon';
 import LeftPanel from './layouts/LeftPanel/LeftPanel';
 import JournalForm from './components/JournalForm/JournalForm';
+import { useLocalStorage } from './hooks/use-localstorage.hook';
+
+function mapItems(items) {
+  if (!items) {
+    return [];
+  }
+  return items.map((i) => ({
+    ...i,
+    date: new Date(i.date),
+  }));
+}
 
 function App() {
-  const [items, setItems] = useState([]);
-
-  useEffect(() => {
-    const data = JSON.parse(localStorage.getItem('data'));
-    if (data) {
-      setItems(
-        data.map((item) => ({
-          ...item,
-          date: new Date(item.date),
-        }))
-      );
-    }
-  }, []);
-
-  useEffect(() => {
-    if (items.length) {
-      localStorage.setItem('data', JSON.stringify(items));
-    }
-  }, [items]);
+  const [items, setItems] = useLocalStorage('data');
 
   const addItem = (item) => {
-    setItems((oldItems) => [
-      ...oldItems,
+    setItems([
+      ...mapItems(items),
       {
         post: item.post,
         title: item.title,
         date: new Date(item.date),
-        id:
-          oldItems.length > 0 ? Math.max(...oldItems.map((i) => i.id)) + 1 : 1,
+        id: items.length > 0 ? Math.max(...items.map((i) => i.id)) + 1 : 1,
       },
     ]);
   };
@@ -47,7 +38,7 @@ function App() {
       <LeftPanel>
         <Header />
         <JournalAddButoon />
-        <JournalList items={items} />
+        <JournalList items={mapItems(items)} />
       </LeftPanel>
       <Body>
         <JournalForm onSubmit={addItem} />
